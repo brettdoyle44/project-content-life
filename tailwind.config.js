b0c9376dea8c4732aab7ef2df2c348b9697749a3
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2021-11-14 00:25:30 -0500

    Added Tailwind and Stitches

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          electric: '#db00ff',
+          ribbon: '#0047ff',
+          purple: {
+              500: '#800080'
+          }
+        },
+      },
+    },
+    plugins: [],
+  }
